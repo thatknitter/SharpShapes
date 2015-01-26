48768bc73df1738a30fb2cf730b23351c0392af2
thatknitter <kblewis.flt@gmail.com>
2015-01-26 10:14:14 -0600

    First tests

---
+++ TestSharpShapes/UnitTestShape.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+using System.Drawing;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestShape
+    {
+        public class MyShape : Shape
+        {
+            public override int SidesCount 
+            {
+                get { throw new NotImplementedException(); }
+            }
+            public override void Scale(int percent)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override decimal Perimeter()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override decimal Area()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void TestSettingBorderColor()
+        {
+            Shape shape = new MyShape();
+            shape.BorderColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.BorderColor);
+        }
+
+        [TestMethod]
+        public void TestSettingDefaultBorderColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Tomato, shape.BorderColor);
+        }
+
+        [TestMethod]
+        public void TestSettingFillColor()
+        {
+            Shape shape = new MyShape();
+            shape.FillColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.FillColor);
+        }
+
+        [TestMethod]
+        public void TestSettingDefaultFillColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Bisque, shape.FillColor);
+        }
+    }
+}
