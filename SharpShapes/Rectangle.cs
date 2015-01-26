4590d76c214574f2a95b2fe564b89312373f7040
thatknitter <kblewis.flt@gmail.com>
2015-01-26 12:01:09 -0600

    tests written

---
+++ SharpShapes/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShapes
+{
+    public class Rectangle : Shape
+    {
+        private int width;
+        public int Width 
+        {
+            get { return this.width; } 
+        }
+
+        private int height;
+        public int Height 
+        {
+            get { return this.height; } 
+        }
+
+        public override int SidesCount
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Scale(int percent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestSharpShapes/UnitTestRectangles.cs
+++ TestSharpShapes/UnitTestRectangles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
 
 namespace TestSharpShapes
 {
@@ -15,17 +16,126 @@ namespace TestSharpShapes
         }
 
         [TestMethod]
-        [ExpectedException(ArgumentException)]
-        public void TestRectangleConstructorSanityChecksValues()
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksWidth()
         {
             Rectangle rectangle = new Rectangle(0, 50);   
         }
 
         [TestMethod]
-        [ExpectedException(ArgumentException)]
-        public void TestRectangleConstructorRequiresValues()
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksWidthPositivity()
         {
-            Rectangle rectangle = new Rectangle();
+            Rectangle rectangle = new Rectangle(-1, 50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksHeight()
+        {
+            Rectangle rectangle = new Rectangle(50, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksHeightPositivity()
+        {
+            Rectangle rectangle = new Rectangle(50, -1);
+        }
+
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void TestRectangleConstructorRequiresValues()
+        //{
+        //    Rectangle rectangle = new Rectangle();
+        //}
+
+        [TestMethod]
+        public void TestScaleRectangle200percent()
+        {
+            Rectangle rectangle = new Rectangle(10, 15);
+            rectangle.Scale(200);
+            Assert.AreEqual(20, rectangle.Width);
+            Assert.AreEqual(30, rectangle.Height);
+        }
+
+        [TestMethod]
+        public void TestScaleRectangle150Percent()
+        {
+            Rectangle rectangle = new Rectangle(10, 15);
+            rectangle.Scale(150);
+            Assert.AreEqual(15, rectangle.Width);
+            Assert.AreEqual(22.5, rectangle.Height);
+        }
+
+        [TestMethod]
+        public void TestScaleRectangle37Percent()
+        {
+            Rectangle rectangle = new Rectangle(10, 15);
+            rectangle.Scale(37);
+            Assert.AreEqual(3.7, rectangle.Width);
+            Assert.AreEqual(4.85, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestScaleRectangleTo0Percent()
+        {
+            Rectangle rectangle = new Rectangle(10, 15);
+            rectangle.Scale(0);
+            
+        }
+
+        [TestMethod]
+        public void TestScaleRectangleTo100percent()
+        {
+            Rectangle rectangle = new Rectangle(10, 15);
+            rectangle.Scale(-5);
+            
+        }
+
+        [TestMethod]
+        public void TestSidesCount()
+        {
+            Rectangle rectangle = new Rectangle(1, 5);
+            Assert.AreEqual(4, rectangle.SidesCount);
+        }
+
+        [TestMethod]
+        public void TestRectangleArea()
+        {
+            Rectangle rectangle = new Rectangle(1, 5);
+            Assert.Equals(5, rectangle.Area());
+        }
+
+        [TestMethod]
+        public void TestBiggerRectangleArea()
+        {
+            Rectangle rectangle = new Rectangle(10, 100);
+            Assert.Equals(1000, rectangle.Area());
+        }
+
+        [TestMethod]
+        public void TestRectanglePerimeter()
+        {
+            Rectangle rectangle = new Rectangle(1, 5);
+            Assert.Equals(12, rectangle.Area());
+        }
+
+        [TestMethod]
+        public void TestBiggerRectanglePerimeter()
+        {
+            Rectangle rectangle = new Rectangle(10, 100);
+            Assert.Equals(220, rectangle.Area());
+        }
+
+        [TestMethod]
+        public void TestDefaultColors()
+        {
+            Rectangle shape = new Rectangle(10, 15);
+            Assert.AreEqual(System.Drawing.Color.Tomato, shape.BorderColor);
+            Assert.AreEqual(System.Drawing.Color.Bisque, shape.FillColor);
+            
         }
     }
 }
