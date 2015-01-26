7e53d6a42895e1e5b2f0680aa7b78d94abe4a9f2
thatknitter <kblewis.flt@gmail.com>
2015-01-26 10:57:27 -0600

    Create rectangle test. WIP

---
+++ TestSharpShapes/UnitTestRectangles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestRectangles
+    {
+        [TestMethod]
+        public void TestRectangleConstructor()
+        {
+            Rectangle rectangle = new Rectangle(40, 50);
+            Assert.AreEqual(40, rectangle.Width);
+            Assert.AreEqual(50, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(ArgumentException)]
+        public void TestRectangleConstructorSanityChecksValues()
+        {
+            Rectangle rectangle = new Rectangle(0, 50);   
+        }
+
+        [TestMethod]
+        [ExpectedException(ArgumentException)]
+        public void TestRectangleConstructorRequiresValues()
+        {
+            Rectangle rectangle = new Rectangle();
+        }
+    }
+}
